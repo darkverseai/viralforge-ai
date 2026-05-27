7ae4c15dbf4bbcf5840877ee4cb37b27e6bdf1b8
darkverseai <mohammadjahid8928@gmail.com>
2026-05-27 16:11:08 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -5,25 +5,24 @@ export default async function handler(req, res) {
     const { message } = req.body;
 
     const response = await fetch(
-
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-
+      "https://api.groq.com/openai/v1/chat/completions",
       {
         method: "POST",
 
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          "Authorization":
+          `Bearer ${process.env.GROQ_API_KEY}`
         },
 
         body: JSON.stringify({
 
-          contents: [
+          model: "llama3-70b-8192",
+
+          messages: [
             {
-              parts: [
-                {
-                  text: message
-                }
-              ]
+              role: "user",
+              content: message
             }
           ]
 
@@ -34,13 +33,11 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    console.log(data);
-
     const reply =
-    data.candidates?.[0]?.content?.parts?.[0]?.text;
+      data?.choices?.[0]?.message?.content;
 
     res.status(200).json({
-      reply: reply || "No AI response"
+      reply: reply || "No response"
     });
 
   } catch (error) {
